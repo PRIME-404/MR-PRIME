128e687115a1e9b57e42fde67715a0be87023b9b
DARK-DEVIL <148872502+AHMED-143@users.noreply.github.com>
2024-06-05 23:15:15 +0600

    Update GREEN.py

--- GREEN.py
+++ GREEN.py
@@ -1,6 +1,2 @@
-import platform
-b = platform.architecture()[0]
-if b == '64bit':
-    import GREEN
-elif b == '32bit':
-    print("32bit Not Supported! Sorry") 
+if __name__=="__main__":
+   import GREEN
